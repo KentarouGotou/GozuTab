151f555e58d64a09befc574e39182cd5ea1148c0
KentarouGotou <gotokentaro0611@keio.jp>
2023-08-06 19:03:18 +0900

    complete convert.py

--- mydataset/convert.py
+++ mydataset/convert.py
@@ -1,12 +1,6 @@
 import os
 import json
-import guitarpro # Note: works only with PyGuitarPro version 0.6
-import guitarpro as gp
-from collections import Counter
-import re
-import json
-import math
-from fractions import Fraction
+import pprint
 import sys
 
 effect_list = {
@@ -15,12 +9,13 @@ effect_list = {
     "harmonic" : "har",
     "vibrato" : "vib",
     "bend" : "bn",
-    # "slide" : "sl",
+    # "slide" : "sld",
     # "hammer" : "h_p",
-    "trill" : "trl"
+    "trill" : "trl",
     # "palm_mute" : "brm",
-    # "staccato" : "st",
+    # "staccato" : "stc",
     # "slap_effect" : "slp"
+    "tie" : "tie"
 }
 
 def translate_trackbeats(track, inst):
@@ -34,7 +29,7 @@ def translate_trackbeats(track, inst):
         effects = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}
         ties = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}
         # process of ach note
-        for string in track[clock]["notes"]:
+        for string in inst_track[clock]["notes"]:
             # only for notes
             if string["token"].split(":")[1] == "note":
                 # position
@@ -48,7 +43,7 @@ def translate_trackbeats(track, inst):
                         pass
                     #tie
                     elif l.split(":")[1] == "tie":
-                        ties[string_number] = "'"
+                        ties[string_number] = 't'
                     #bending
                     elif l.split(":")[1] == "bend":
                         #searching max of bend degree
@@ -71,162 +66,102 @@ def translate_trackbeats(track, inst):
                     #other effects
                     else:
                         effects[string_number] = effect_list[l.split(":")[1]]
-        duration = track[clock]["duration"]
+        duration = inst_track[clock]["duration"]
         clock_notes["position"] = position
         clock_notes["effects"] = effects
+        clock_notes["ties"] = ties
         clock_notes["duration"] = duration
         measure_note.append(clock_notes)
     return measure_note
 
 def translate_all_measure(content, inst):
-    time = 960
-    repeat_clock = 0
+    # {
+    #     "trackbeats":{
+    #         "clean0": {
+    #             "960": {
+    #                 "bfx": [],
+    #                 "notes": [
+    #                     {
+    #                         "token": "clean0:note:s1:f4",
+    #                         "nfx":[
+    #                               "nfx:staccato",
+    #                               "nfx:bend:type1:pos0:val0:vib0:pos6:val4:vib0:pos12:val4:vib0"
+    #                             ]
+    #                         },
+    #                     {
+    #                         "token": "clean0:note:s5:f4",
+    #                         "nfx": [
+    #                             "nfx:slide:1"
+    #                         ]
+    #                     }
+    #                     ],
+    #                 "duration": 960
+    #                 },
+    #             "1920": {...}
+    #             }
+    #         },
+    #     "measure_tokens":[],
+    #     "clock": 960
+    # }
     all_notes = []
+    measure_index = 0
+    repeat_close_clocks = []
     #process each measure
-    for measure in content:
-        # {
-        #     "trackbeats":{
-        #         "clean0": {
-        #             "960": {
-        #                 "bfx": [],
-        #                 "notes": [
-        #                     {
-        #                         "token": "clean0:note:s1:f4",
-        #                         "nfx":[
-        #                               "nfx:staccato",
-        #                               "nfx:bend:type1:pos0:val0:vib0:pos6:val4:vib0:pos12:val4:vib0"
-        #                             ]
-        #                         },
-        #                     {
-        #                         "token": "clean0:note:s5:f4",
-        #                         "nfx": [
-        #                             "nfx:slide:1"
-        #                         ]
-        #                     }
-        #                     ],
-        #                 "duration": 960
-        #                 },
-        #             "1920": {...}
-        #             }
-        #         },
-        #     "measure_tokens":[],
-        #     "clock": 960
-        # }
+    while measure_index < len(content):
+        measure = content[measure_index]
         
         # measure_clock
         measure_clock = measure["clock"]
         
+        # trackbeats
+        measure_notes = translate_trackbeats(measure["trackbeats"], inst)
+        for measure_note in measure_notes:
+            all_notes.append(measure_note)
+        
         # measure_tokens
         measure_tokens = measure["measure_tokens"]
-        repeat_open_clock = 0
-        repeat_close = False
-        repeat_alternative = 0
+        repeat_close_clock = False
         for measure_token in measure_tokens:
             if measure_token.split(":")[1] == "repeat_open":
                 repeat_open_clock = measure_clock
             elif measure_token.split(":")[1] == "repeat_close":
-                repeat_close = True
-            elif measure_token.split(":")[1] == "repeat_alternative":
-                
+                repeat_close_clock = True
         
-        # trackbeats
-        notes_moment = translate_trackbeats(measure["trackbeats"], inst)
-        all_notes.append(notes_moment)
-                
+        # update measure_index
+        if repeat_close_clock and measure_clock not in repeat_close_clocks:
+            for i in range(measure_index, -1, -1):
+                if content[i]["clock"] == repeat_open_clock:
+                    measure_index = i
+                    repeat_close_clock = False
+                    repeat_close_clocks.append(measure_clock)
+                    break
+        else:
+            measure_index += 1
+    
+    return all_notes
         
 def main():
+    usage = """
+If you want to convert all json files in the folder, please use this command:
+    python convert.py [folder name] [instrument] [output folder name]
+If you want to convert json file, please use this command:
+    python convert.py [json file] [instrument] [output file]
+    ex) python convert.py labels/test2.json clean0 test2.json
+    """
     #load json file
-    file = "labels/test2.json"
+    file = sys.argv[1]
     data = open(file, "r")
     load = json.load(data)
     #seperate head and content
     head = load[0]
     content = load[1:]
-    all_data = []
     print(head)
     #translate content
-    for measure in content:
-        # {
-        #     "trackbeats":{
-        #         "clean0": {
-        #             "960": {
-        #                 "bfx": [],
-        #                 "notes": [
-        #                     {
-        #                         "token": "clean0:note:s1:f4",
-        #                         "nfx":[
-        #                               "nfx:staccato",
-        #                               "nfx:bend:type1:pos0:val0:vib0:pos6:val4:vib0:pos12:val4:vib0"
-        #                             ]
-        #                         },
-        #                     {
-        #                         "token": "clean0:note:s5:f4",
-        #                         "nfx": [
-        #                             "nfx:slide:1"
-        #                         ]
-        #                     }
-        #                     ],
-        #                 "duration": 960
-        #                 },
-        #             "1920": {...}
-        #             }
-        #         },
-        #     "measure_tokens":[],
-        #     "clock": 960
-        # }
-        # notes of clean0
-        track = measure["trackbeats"]["clean0"]
-        # process at each moment
-        for clock in track:
-            position = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}
-            effects = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}
-            ties = {"1": "-", "2": "-", "3": "-", "4": "-", "5": "-", "6": "-"}
-            # process of ach note
-            for k in track[clock]["notes"]:
-                # only for notes
-                if k["token"].split(":")[1] == "note":
-                    # position
-                    string = k["token"].split(":")[2][1:]
-                    fret = int(k["token"].split(":")[3][1:])
-                    position[string] = fret
-                    # effects
-                    nfx = k["nfx"]
-                    for l in nfx:
-                        if l.split(":")[1] not in effect_list.keys():
-                            pass
-                        #tie
-                        elif l.split(":")[1] == "tie":
-                            ties[string] = "'"
-                        #bending
-                        elif l.split(":")[1] == "bend":
-                            #searching max of bend degree
-                            max_bend_degree = 0
-                            for m in l.split(":"):
-                                if m[:3] == "val" and int(m[3:]) > max_bend_degree:
-                                    max_bend_degree = int(m[3:])
-                            #rounding degree
-                            # half-tone = 4, whole-tone = 8, max-tone = 12
-                            if max_bend_degree <= 6:
-                                effects[string] = effect_list["bend"] + "1"
-                            elif 6 < max_bend_degree <= 10:
-                                effects[string] = effect_list["bend"] + "2"
-                            else:
-                                effects[string] = effect_list["bend"] + "3"
-                        #trill
-                        elif l.split(":")[1] == "trill":
-                            fret_distance = l.split(":")[2][4:]
-                            effects[string] = effect_list["trill"] + fret_distance
-                        #other effects
-                        else:
-                            effects[string] = effect_list[l.split(":")[1]]
-            duration = track[clock]["duration"]
-            print("clock", clock)
-            print("position", position)
-            print("effects", effects)            
-            print("duration", duration)
-            
-                
-            
+    inst = sys.argv[2]
+    all_notes = translate_all_measure(content, inst)
+    #write json file
+    with open(sys.argv[3], "w") as f:
+        json.dump(all_notes, f, indent=4)
 
 if __name__ == "__main__":
     main()
