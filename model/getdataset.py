d21096bfab2461e45c00d9a0abf6c8136bebcdbb
KentarouGotou <92863589+KentarouGotou@users.noreply.github.com>
2023-10-09 03:00:38 +0900

    making model

--- model/getdataset.py
+++ model/getdataset.py
@@ -4,22 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Dataset(data.Dataset):
-    def __init__(self, data_dir_path="preprocessed/", mode="cqt", context_window=9, plot=False):
+    def __init__(self, data_dir_path="preprocessed/", mode="cqt", plot=False):
         self.data_dir_path = data_dir_path
         self.mode = mode
-        self.context_window = context_window
-        self.half_context_window = int(self.context_window / 2)
         self.plot = plot
         
         # data paths
         self.tab_data_paths = self.get_tab_data_paths()
         self.audio_data_paths = self.get_audio_data_paths()
         
-        # full audio length
+        # audio max length
         if self.mode == "raw_wave":
             self.full_audio_length = 5000000
         else:
-            self.full_audio_length = 8000
+            self.full_audio_length = 8192
+        # tab max length
+        self.full_tab_length = 512
+        # padding tab
+        self.padding_tab = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0]
         
     def get_tab_data_paths(self):
         data_paths = []
@@ -49,23 +51,31 @@ class Dataset(data.Dataset):
         loaded_audio_data = np.load(audio_data_path)
         
         # generate audio data
-        audio_data = np.empty((self.full_audio_length, loaded_audio_data.shape[0], self.context_window))
+        if self.mode == "raw_wave":
+            audio_data = np.pad(loaded_audio_data, [(0, self.full_audio_length - loaded_audio_data.shape[0])])
+            # expand dimension
+            audio_data = np.expand_dims(audio_data, axis=1)
+            # transpose
+            audio_data = np.transpose(audio_data, (1, 0))
+        else:
+            audio_data = np.pad(loaded_audio_data, [(0, 0), (0, self.full_audio_length - loaded_audio_data.shape[1])])
+            # expand dimension
+            audio_data = np.expand_dims(audio_data, axis=2)
+            # transpose
+            audio_data = np.transpose(audio_data, (2, 0, 1))
+        
+        # generate tab data
         # padding
-        full_audio_data = np.pad(loaded_audio_data, ((0, 0), (0, self.full_audio_length - loaded_audio_data.shape[1])))
-        padd_audio_data = np.pad(full_audio_data, ((0, 0), (self.half_context_window, self.half_context_window)))
-        # insert
-        for i in range(self.full_audio_length):
-            audio_data[i] = padd_audio_data[:, i:i+self.context_window]
-        # expand dim
-        audio_data = np.expand_dims(audio_data, -1)
+        padding_tab_data = [self.padding_tab for i in range(self.full_tab_length - tab_data.shape[0])]
+        tab_data = np.concatenate([tab_data, padding_tab_data])
         
         # plot
-        print(tab_data.shape, audio_data.shape)
-        # Tab data
-        print("Tab data: ", tab_data[0])
-        # Audio data
         if self.plot:
+            print("Tab data shape: ", tab_data.shape, "Audio data shape: ", audio_data.shape)
+            # Tab data
+            print("Tab data: ", tab_data[0])
+            # Audio data
             plt.figure(figsize=(16, 9))
-            plt.imshow(full_audio_data[:,1000:1200], cmap="jet")
+            plt.imshow(audio_data[1000:1200], cmap="jet")
             plt.show()
         return tempo, tab_data, audio_data
